0bb32c71ec2e1526fd19558b6412b042aef38b2c
Brandon Lyons <brandon1.lyons@ge.com>
2019-09-25 14:48:02 -0400

    Bump version

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 function startApp({ pageTitle, appName }) {
+  app.set('views', './views')
   app.set("view engine", "pug");
   app.get("/index.html", (req, res) => {
     res.render("index", { pageTitle, appName });
