a19881a3cf7f9fbf104f9bec8c4acea4f9431c00
Brandon Lyons <brandon1.lyons@ge.com>
2019-09-25 14:44:41 -0400

    Initial commit with readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# We Moved
+
+### Installation
+`npm install we-moved`
+
+### Usage Instructions
+```
+const startApp = require('we-moved')
+
+const config = {
+  pageTitle: 'We Moved!',
+  appName: 'MOA'
+}
+
+startApp(config)
+```
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+function startApp({ pageTitle, appName }) {
+  app.set("view engine", "pug");
+  app.get("/index.html", (req, res) => {
+    res.render("index", { pageTitle, appName });
+  });
+
+  app.get("*", (req, res) => res.redirect("/index.html"));
+
+  app.listen(port, () => console.log(`App listening on port: ${port}`));
+}
+
+module.exports = startApp
